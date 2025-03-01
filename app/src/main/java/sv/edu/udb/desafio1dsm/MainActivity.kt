86bf21143eca2cc5dd6ba1b9f211bf4dc4132f25
YensyC <ale_barahona93@hotmail.com>
2025-02-28 22:10:57 -0600

    botones

--- app/src/main/java/sv/edu/udb/desafio1dsm/MainActivity.kt
+++ app/src/main/java/sv/edu/udb/desafio1dsm/MainActivity.kt
@@ -1,10 +1,16 @@
 package sv.edu.udb.desafio1dsm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+lateinit var btnCalculoSalario: Button
+lateinit var btnCalculoPromedio: Button
+lateinit var btnCalculadora: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Inicializar Botones
+        btnCalculoSalario = findViewById(R.id.btnCalculoSalario)
+        btnCalculoPromedio = findViewById(R.id.btnCalculoPromedio)
+        btnCalculadora = findViewById(R.id.btnCalculadora)
+
+        //Indicando acciones para Botones
+        btnCalculoSalario.setOnClickListener {
+            val intent = Intent(this, CalculoSalario::class.java)
+            startActivity(intent)
+        }
+
+        btnCalculoPromedio.setOnClickListener {
+            val intent = Intent(this, CalculoPromedio::class.java)
+            startActivity(intent)
+        }
+
+        btnCalculadora.setOnClickListener {
+            val intent = Intent(this, Calculadora::class.java)
+            startActivity(intent)
         }
     }
 }
